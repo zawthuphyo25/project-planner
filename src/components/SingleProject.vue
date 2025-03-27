2273c51056b85057cedaa02187e6091079863557
zawthuphyo <ztphyo101@gmail.com>
2025-03-27 13:36:24 +0700

    first commit

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="project">
+    <div class="flexing">
+      <h3 @click="showDetail = !showDetail">{{ project.title }}</h3>
+      <div>
+        <i class="ri-delete-bin-2-fill icon"></i>
+        <i class="ri-pencil-fill icon"></i>
+        <i class="ri-check-fill icon"></i>
+      </div>
+    </div>
+
+    <p v-if="showDetail">{{ project.detail }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["project"],
+  data() {
+    return {
+      showDetail: false,
+    };
+  },
+};
+</script>
+
+<style>
+.project {
+  padding: 20px;
+  background-color: #f2f2f2;
+  margin: 10px;
+  border-left: 8px solid crimson;
+  border-radius: 8px;
+}
+h3 {
+  color: indigo;
+  cursor: pointer;
+}
+.flexing {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.icon {
+  margin-left: 10px;
+  cursor: pointer;
+  transition: 0.3s ease-in-out;
+}
+.icon:hover {
+  color: #777;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <div v-for="project in projects" :key="project.id">
+      <!-- {{ project }} -->
+      <SingleProject :project="project"></SingleProject>
+    </div>
+  </div>
+</template>
+
+<script>
+import SingleProject from "../components/SingleProject";
+
+export default {
+  name: "HomeView",
+  components: {
+    SingleProject,
+  },
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/projects")
+      .then((response) => {
+        // console.log(response);
+        return response.json();
+      })
+      .then((datas) => {
+        // console.log(datas);
+        this.projects = datas;
+      })
+      .catch(() => {});
+  },
+};
+</script>
