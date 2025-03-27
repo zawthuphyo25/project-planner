b09d769b3886fe27c7bdd17368665e429f3ca37b
zawthuphyo <ztphyo101@gmail.com>
2025-03-28 00:08:48 +0700

    filter nav feature

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <div v-for="project in projects" :key="project.id">
+    <FilterNav @filterValue="current = $event" :current="current"></FilterNav>
+    <div v-for="project in filterProjects" :key="project.id">
       <!-- {{ project }} -->
       <SingleProject
         :project="project"
@@ -13,16 +14,19 @@
 </template>
 
 <script>
+import FilterNav from "../components/FilterNav";
 import SingleProject from "../components/SingleProject";
 
 export default {
   name: "HomeView",
   components: {
+    FilterNav,
     SingleProject,
   },
   data() {
     return {
       projects: [], //[{},{},{}]
+      current: "all",
     };
   },
   methods: {
@@ -36,6 +40,21 @@ export default {
         return project === id;
       });
       findProject.complete = !findPrject.commplete;
+    },
+  },
+  computed: {
+    filterProjects() {
+      if (this.current === "complete") {
+        return this.projects.filter((p) => {
+          return p.complete;
+        });
+      }
+      if (this.current === "ongoing") {
+        return this.projects.filter((p) => {
+          return !p.complete;
+        });
+      }
+      return this.projects;
     },
   },
   mounted() {
